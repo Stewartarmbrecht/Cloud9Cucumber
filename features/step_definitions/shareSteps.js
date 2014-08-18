24147961aa0bd38c38e3b3ec67728fb3b44567e9
Stewart Armbrecht <stewart.armbrecht@protiviti.com>
2014-08-18 17:52:34 +0000

    Test

---
+++ features/step_definitions/shareSteps.js
@@ -0,0 +1,17 @@
+var shareSteps = module.exports = function() {
+
+    this.Given(/^I perform an action$/, function(callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.When(/^I peroform an action with a breakpoints$/, function(callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.Then(/^the application should break on the breakpoint$/, function(callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+}
---
+++ features/test.feature
@@ -0,0 +1,10 @@
+Feature: Test Feature
+  In order to debug my test code
+  As a tester
+  I would like to have the application stop on breakpoints
+  
+  Scenario: Breakpoint Test
+    Given I perform an action
+    When I peroform an action with a breakpoints
+    Then the application should break on the breakpoint
+  
